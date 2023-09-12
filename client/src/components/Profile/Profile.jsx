f87d1852da8347687314542c6560a6f4085703f3
Vitaly Kryanin <krianinvv@internet.ru>
2023-09-12 17:13:57 +0300

    add to changed avatar

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -1,58 +1,25 @@
 import React, { useContext, useEffect, useRef, useState } from "react";
 import { CurrentUserContext } from "../../context/CurrentUserContext";
 import { Header } from "../Header/Header";
-
-import { ProfileForm } from "./ProfileForm/ProfileForm";
-import { ProfileContainer } from "./ProfileContainer/ProfileContainer";
 import { ProfileEdit } from "./ProfileEdit/ProfileEdit";
+import { ProfileForm } from "./ProfileForm/ProfileForm";
 
 export const Profile = ({ handleGetAvatar, logout, handleAddAvatar, renderComponent }) => {
     const currentUser = useContext(CurrentUserContext);
-    const [image, setImage] = useState(null);
-    const imageInputRef = useRef(null);
 
     useEffect(() => {
         renderComponent();
     }, []);
-
-    const loadImage = (e) => {
-        const img = e.target.files[0];
-        setImage(img);
-    };
-
-    const handleImageClick = () => {
-        imageInputRef.current.click();
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        handleAddAvatar({ userId: currentUser.id, img: image });
-    };
 
     return (
         <>
             <Header loggedIn={currentUser.loggedIn} logout={logout} />
             <main className="profile">
                 <div className="profile__content">
-                    <div className="profile__header">
-                        <ProfileContainer
-                            handleImageClick={handleImageClick}
-                            currentUser={currentUser}
-                        />
-                        <h1 className="profile__title" >
-                            Привет, <br />{currentUser.name}!
-                        </h1>
-                        <ProfileForm
-                            loadImage={loadImage}
-                            handleSubmit={handleSubmit}
-                            imageInputRef={imageInputRef}
-                        />
-                    </div>
-                    < ProfileEdit/>
+                    < ProfileForm handleAddAvatar={handleAddAvatar} currentUser={currentUser} />
+                    <ProfileEdit />
                 </div>
-                <div className="profile__basket">
-                    test2
-                </div>
+                <div className="profile__basket">test2</div>
             </main>
         </>
     );
--- client/src/components/Profile/ProfileContainer/ProfileContainer.jsx
+++ client/src/components/Profile/ProfileContainer/ProfileContainer.jsx
@@ -1,30 +1,9 @@
-import editAvatar from '../../../image/edit_avatar.svg';
-import pepe from '../../../image/pepe.jpg'
 
-export const ProfileContainer = ({handleImageClick, currentUser}) => {
-    return (
-        <div
-            className="profile__avatar-container"
-            onClick={handleImageClick}
-        >
-            {currentUser.avatar
-                ? <img
-                    className="profile__avatar"
-                    src={`${process.env.REACT_APP_API_URL}/${currentUser.avatar}`}
-                    alt="Аватар пользователя"
-                />
-                : <img
-                    className="profile__avatar"
-                    src={pepe}
-                    alt="Стандарный аватар"
-                />
-            }
-            <div className="profile__avatar-overlay">
-                <img
-                    src={editAvatar}
-                    alt="Изменить аватар"
-                />
-            </div>
-        </div>
+
+export const ProfileContainer = ({ handleImageClick, currentUser, handleSubmit, profileFormRef, loadImage, imageInputRef }) => {
+    return (<>
+        
+    </>
+
     )
 }
