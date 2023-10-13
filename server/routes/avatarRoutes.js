e6e17a721b30cc0284bf8dafdd2c88a9fc14d57e
Vitaly Kryanin <92264488+VKryanin@users.noreply.github.com>
2023-10-13 14:12:39 +0300

    Delete server directory

--- server/routes/avatarRoutes.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express')
-const router = new Router;
-const checkRole = require('../middleware/checkRoleMiddleware');
-const avatarControllers = require('../controllers/avatarControllers');
-
-router.post('/', avatarControllers.createAvatar);
-router.get('/', avatarControllers.getAvatar);
-
-module.exports = router
--- server/routes/ratingRouter.js
+++
@@ -1,7 +0,0 @@
-const Router = require('express')
-const router = new Router;
-const ratingController = require('../controllers/ratingController');
-
-router.post('/', ratingController.addRating);
-
-module.exports = router
